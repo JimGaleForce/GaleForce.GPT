f359b670bdb23be1778cca2d07f46ef18b3e93c9
Jim Gale <jimgale@microsoft.com>
2023-05-19 19:45:39 -0700

    added retry

--- GPTFunction.cs
+++ GPTFunction.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
     using OpenAILib;
 
@@ -41,19 +42,22 @@
             return this;
         }
 
-        public async Task<string> ActOn(string prompt)
+        public async Task<string> ActOn(string prompt, int retries = int.MinValue)
         {
             if (this.Service == null)
             {
                 throw new Exception("Missing service");
             }
 
-            return await this.Service.Do(this.On(prompt));
+            return await this.Service.Do(this.On(prompt), retries);
         }
     }
 
     public class GPTService
     {
+        public static int DefaultRetries = 3;
+        public static int DefaultRetryDelayMS = 1000;
+
         public GPTService(AzureOpenAIClient azureOpenAIClient)
         {
             this.AzureOpenAIClient = azureOpenAIClient;
@@ -70,14 +74,25 @@
 
         public bool UseInstructionsInPrompt { get; set; } = false;
 
-        public async Task<string> Do(GPTFunction function)
+        public Func<string, string, string> FakeReturn { get; set; } //instructions, prompt, return
+
+        public async Task<string> Do(GPTFunction function, int retries = int.MinValue)
         {
+            if (retries == int.MinValue)
+            {
+                retries = DefaultRetries;
+            }
+
             var sequence = new List<ChatMessage>();
             var prompt = this.UseInstructionsInPrompt
-                ?
-                (function.Instructions ?? "") + "\n" + function.Prompt
+                ? (function.Instructions ?? string.Empty) + "\n" + function.Prompt
                 : function.Prompt;
             var inst = this.UseInstructionsInPrompt ? null : function.Instructions;
+
+            if (this.FakeReturn != null)
+            {
+                return this.FakeReturn(inst, prompt);
+            }
 
             if (!string.IsNullOrEmpty(inst))
             {
@@ -88,15 +103,41 @@
 
             if (this.AzureOpenAIClient != null)
             {
-                return await this.AzureOpenAIClient.GetChatCompletionAsync(sequence);
+                return await this.Retry(
+                    () => this.AzureOpenAIClient.GetChatCompletionAsync(sequence),
+                    retries);
             }
 
             if (this.OpenAIClient != null)
             {
-                return await this.OpenAIClient.GetChatCompletionAsync(sequence);
+                return await this.Retry(
+                    () => this.OpenAIClient.GetChatCompletionAsync(sequence),
+                    retries);
             }
 
             return null;
+        }
+
+        private async Task<string> Retry(Func<Task<string>> func, int retries)
+        {
+            Exception e = null;
+            var count = retries;
+            while (count-- > 0)
+            {
+                try
+                {
+                    var result = await func();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(1000); //todo: pass in service default
+                    e = ex;
+                }
+            }
+
+            throw e;
         }
     }
 }
